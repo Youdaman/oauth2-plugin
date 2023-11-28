8cc5f6d03f90ebebe7dd19bd32ab33db3689712c
Stew Heckenberg <stewart.heckenberg@gmail.com>
2023-11-28 18:58:49 +0800

    permission callback based on scopes

--- oauth2-plugin.php
+++ oauth2-plugin.php
@@ -62,30 +62,83 @@ class OAuth2_Plugin {
 				Field::make( 'complex', 'scopes', 'Scopes' )
 					->add_fields( array(
 						Field::make( 'text', 'name', 'Name' ),
-						Field::make( 'complex', 'endpoints', 'Endpoints' )
-							->add_fields( array(
-								// Field::make( 'text', 'url', 'URL' ),
-								Field::make( 'select', 'url', 'URL' )
-									->add_options( array( $this, 'get_routes' ) ),
-								Field::make( 'select', 'method', 'Method' )
-									->add_options( array( 'GET', 'POST', 'DELETE', 'PATCH', 'PUT' ) ),
-							)),
+						Field::make( 'multiselect', 'users', 'Users' )
+							->add_options( array( $this, 'get_users' ) ),
+						Field::make( 'multiselect', 'routes', 'Routes' )
+							->add_options( array( $this, 'get_routes' ) ),
+						Field::make( 'multiselect', 'methods', 'Methods' )
+							->add_options( array(
+								'GET' => 'GET',
+								'POST' => 'POST',
+								'DELETE' => 'DELETE',
+								'PATCH' => 'PATCH',
+								'PUT' => 'PUT',
+							) ),
 					)),
 			) );
 	}
 
+	public function get_users() {
+		// associative array of users with id => name
+		$users = array();
+		// get all users
+		$wp_users = get_users();
+		// loop through users
+		foreach ( $wp_users as $wp_user ) {
+			// add user to users array
+			$users[ $wp_user->ID ] = $wp_user->display_name;
+		}
+		return $users;
+	}
+
 	public function get_routes() {
-		return array_keys( rest_get_server()->get_routes() );
+		// associative array of routes with route => route
+		$routes = array();
+		// get all routes
+		$wp_routes = rest_get_server()->get_routes();
+		// loop through routes
+		foreach ( $wp_routes as $wp_route => $wp_route_data ) {
+			// add route to routes array
+			$routes[ $wp_route ] = $wp_route;
+		}
+		return $routes;
 	}
 
 	public function action_rest_api_init() {
+
 		register_rest_route( $this->route_namespace, '/hello', array(
 			'methods'  => \WP_REST_Server::READABLE,
 			'callback' => array( $this, 'route_hello' ),
-			// 'permission_callback' => '__return_true',
 			'permission_callback' => function () {
-				return current_user_can( 'edit_others_posts' );
+
+				$scopes = carbon_get_theme_option( 'scopes' );
+
+				$current_user_id = get_current_user_id();
+				// $current_route = $_SERVER['REQUEST_URI'];
+				$current_route = '/' . $this->route_namespace . '/hello';
+				$current_method = $_SERVER['REQUEST_METHOD'];
+
+				// // phpcs:disable
+				error_log( 'current_user_id: ' . $current_user_id );
+				error_log( 'current_route: ' . $current_route );
+				error_log( 'current_method: ' . $current_method );
+				// error_log( 'scopes: ' . print_r( $scopes, true ) );
+				// // phpcs:enable
+
+				// filter scopes by current user and current route
+				$filtered_scopes = array_filter( $scopes, function ( $scope ) use ( $current_user_id, $current_route, $current_method ) {
+					return in_array( $current_user_id, $scope['users'], true )
+						&& in_array( $current_route, $scope['routes'], true )
+						&& in_array( $current_method, $scope['methods'], true );
+				} );
+				return ! empty( $filtered_scopes );
 			},
+		));
+
+		register_rest_route( $this->route_namespace, '/scopes', array(
+			'methods'  => \WP_REST_Server::READABLE,
+			'callback' => fn() => rest_ensure_response( carbon_get_theme_option( 'scopes' ) ),
+			'permission_callback' => '__return_true',
 		));
 	}
 
